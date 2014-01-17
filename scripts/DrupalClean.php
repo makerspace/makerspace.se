e5bce1b8f08461fa57d22af10bddd5adfdd35bcf
Christian Antila <tagged-github.com@chille.se>
2014-01-17 23:16:40 +0100

    The DrupalClean.php shell script should get the database settings from Drupal's settings.php and not be hardcoded to my local MySQL's root password lol.

--- scripts/DrupalClean.php
+++ scripts/DrupalClean.php
@@ -1,11 +1,11 @@
 #!/usr/bin/php
 <?php
-$db = new Mysqli('localhost', 'root', 'rQ?+BhD,Z9`*_L-bsDHMh2}Zm') or die(':(');;
-//$db->select_db('external') or die(':((');
-$db->select_db('makerspace.se') or die(':((');
+include(__DIR__ . '/../public_html/sites/default/settings.php');
+
+$db = new Mysqli($databases['default']['default']['host'], $databases['default']['default']['username'], $databases['default']['default']['password']) or die(':(');;
+$db->select_db($databases['default']['default']['database']) or die(':((');
 
 $db->query('TRUNCATE TABLE cache');
-
 $db->query('TRUNCATE TABLE cache_block');
 $db->query('TRUNCATE TABLE cache_bootstrap');
 $db->query('TRUNCATE TABLE cache_ckeditor');
@@ -17,15 +17,11 @@ $db->query('TRUNCATE TABLE cache_tags');
 $db->query('TRUNCATE TABLE cache_toolbar');
 $db->query('TRUNCATE TABLE cache_views_info');
 
-/*
-system('rm /home/chille/public_html/sites/default/files/js/*.js');
-system('rm /home/chille/public_html/sites/default/files/js/*.js.gz');
-system('rm /home/chille/public_html/sites/default/files/css/*.css');
-system('rm /home/chille/public_html/sites/default/files/css/*.css.gz');
-*/
-
-system('rm -rf /home/chille/public_html/sites/default/files/php/');
+// Should any of the follwing be cleaned?
 // router
 // sessions
 // config_snapshop
+//system('rm /home/chille/public_html/sites/default/files/js/*');
+//system('rm /home/chille/public_html/sites/default/files/css/*');
 
+system('rm -rf /home/chille/public_html/sites/default/files/php/');
