5b0446fc7a22f18e5a77e8937b9139080da28dce
Christian Antila <tagged-github.com@chille.se>
2014-01-19 04:40:17 +0100

    Updated script that copies new versions of Drupal 8 core into public_html/

---
+++ Scripts/DrupalUpdateCore.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+$drupal8     = __DIR__ .'/../Drupal8';
+$public_html = __DIR__ .'/../public_html';
+
+// Remove old core
+system("rm -rf {$public_html}/composer.json");
+system("rm -rf {$public_html}/composer.lock");
+system("rm -rf {$public_html}/core/");
+system("rm -rf {$public_html}/index.php");
+
+// Load new core
+system("cp -r {$drupal8}/core/ {$public_html}/");
+system("cp -r {$drupal8}/index.php {$public_html}/");
+
+// Check if there is a diff in robots.txt
+system("diff {$drupal8}/robots.txt {$public_html}/robots.txt");
