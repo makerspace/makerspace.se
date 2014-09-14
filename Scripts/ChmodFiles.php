416b7139b215ab3993a2a674ad9187af881a1d11
Christian Antila <tagged-github.com@chille.se>
2014-09-14 19:43:37 +0200

    Updated script used to chmod/chown all files in public_html/

--- Scripts/ChmodFiles.php
+++ Scripts/ChmodFiles.php
@@ -4,7 +4,7 @@ include(__DIR__ . '/../public_html/sites/default/settings.php');
 
 $basedir = __DIR__ . '/../public_html/';
 
-system("chown -R chille:staging {$basedir}");
-system("chmod -R 755 {$basedir}");
-system("chmod -R 775 {$basedir}/sites/default/files");
-system("find {$basedir} -type d -exec chmod +s {} \;");
+system("chown -R staging:staging {$basedir}");
+system("chmod -R 664 {$basedir}");
+system("chmod -R 664 {$basedir}/sites/default/files");
+system("find {$basedir} -type d -exec chmod +xs {} \;");
